b58c80d9f0112a4289c3cdd4e4cdc407a9888e9f
Martin Blomberg <martin.blomberg@outlook.com>
2025-10-26 15:36:37 +0100

    Test

--- src/main/java/com/example/HelloModel.java
+++ src/main/java/com/example/HelloModel.java
@@ -10,6 +10,6 @@ public class HelloModel {
     public String getGreeting() {
         String javaVersion = System.getProperty("java.version");
         String javafxVersion = System.getProperty("javafx.version");
-        return "Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".";
+        return "Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ". Wow!";
     }
 }
---
+++ src/test/java/com/example/HelloModelTest.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HelloModelTest {
+    @Test
+    public void fail() {
+        Assertions.fail();
+    }
+}
