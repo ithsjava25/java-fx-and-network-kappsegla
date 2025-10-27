1f9efe8914ffa306e3d95410c42d6d6ca9a0068b
Martin Blomberg <martin.blomberg@outlook.com>
2025-10-27 13:46:48 +0100

    First animation

--- src/main/java/com/example/HelloController.java
+++ src/main/java/com/example/HelloController.java
@@ -1,7 +1,16 @@
 package com.example;
 
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.util.Duration;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 /**
  * Controller layer: mediates between the view (FXML) and the model.
@@ -14,9 +23,32 @@ public class HelloController {
     private Label messageLabel;
 
     @FXML
+    private Label currentDateAndTime;
+
+    public Button updateButton;
+
+    private Timeline timeline;
+
+    @FXML
     private void initialize() {
         if (messageLabel != null) {
             messageLabel.setText(model.getGreeting());
         }
+        if (currentDateAndTime != null)
+            currentDateAndTime.setText(LocalDateTime.now()
+                    .format(DateTimeFormatter.ofPattern("yyyy-MM-dd hh:mm:ss")));
+
+        timeline = new Timeline(new KeyFrame(Duration.seconds(1), event -> {
+            // Update UI directly
+            currentDateAndTime.setText(LocalDateTime.now()
+                    .format(DateTimeFormatter.ofPattern("yyyy-MM-dd hh:mm:ss")));
+        }));
+        timeline.setCycleCount(Animation.INDEFINITE); // Repeat forever
+        timeline.play();
+    }
+
+    public void stopButtonAction(ActionEvent actionEvent) {
+        timeline.stop();
+        updateButton.setDisable(true);
     }
 }
