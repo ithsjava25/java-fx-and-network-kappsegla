d647c93449a5185dc3ddb11b42fc2087a6713891
Martin Blomberg <martin.blomberg@outlook.com>
2025-10-28 10:51:28 +0100

    Animation of images

--- src/main/java/com/example/HelloController.java
+++ src/main/java/com/example/HelloController.java
@@ -1,7 +1,11 @@
 package com.example;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.util.Duration;
 
 /**
  * Controller layer: mediates between the view (FXML) and the model.
@@ -15,10 +19,34 @@ public class HelloController {
 
     @FXML
     private void initialize() {
+        startNextEvent();
+    }
 
+    private void startNextEvent() {
+        new Timeline(
+                new KeyFrame(
+                        Duration.millis(3000 * Math.random()), event -> {
+                    model.changeImage(
+                            (int) (3 * Math.random()),
+                            (int) (4 * Math.random()));
+                    startNextEvent();
+                }
+                )).play();
     }
 
     public HelloModel getModel() {
         return model;
+    }
+
+    public void image1(MouseEvent mouseEvent) {
+        model.smash(0);
+    }
+
+    public void image2(MouseEvent mouseEvent) {
+        model.smash(1);
+    }
+
+    public void image3(MouseEvent mouseEvent) {
+        model.smash(2);
     }
 }
--- src/main/java/com/example/HelloModel.java
+++ src/main/java/com/example/HelloModel.java
@@ -1,5 +1,7 @@
 package com.example;
 
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
 import javafx.scene.image.Image;
 
 /**
@@ -7,19 +9,94 @@ import javafx.scene.image.Image;
  */
 public class HelloModel {
 
-    Image noSmash;
-    Image smash1;
-    Image smash2;
-    Image smash3;
-    Image smash4;
+    private final Image noSmash;
+    private final Image smash1;
+    private final Image smash2;
+    private final Image smash3;
+    private final Image smash4;
+
+    private final ObjectProperty<Image> first;
+    private final ObjectProperty<Image> second;
+    private final ObjectProperty<Image> third;
 
     public HelloModel() {
         noSmash = new Image(getClass()
                 .getResource("/lantern.png")
                 .toExternalForm());
+        smash1 = new Image(getClass()
+                .getResource("/lantern1.png")
+                .toExternalForm());
+        smash2 = new Image(getClass()
+                .getResource("/lantern2.png")
+                .toExternalForm());
+        smash3 = new Image(getClass()
+                .getResource("/lantern3.png")
+                .toExternalForm());
+        smash4 = new Image(getClass()
+                .getResource("/lantern4.png")
+                .toExternalForm());
+        first = new SimpleObjectProperty<>(noSmash);
+        second = new SimpleObjectProperty<>(noSmash);
+        third = new SimpleObjectProperty<>(noSmash);
     }
 
-    public Image getNoSmash() {
-        return noSmash;
+    public Image getFirst() {
+        return first.get();
+    }
+
+    public ObjectProperty<Image> firstProperty() {
+        return first;
+    }
+
+    public void setFirst(Image first) {
+        this.first.set(first);
+    }
+
+    public Image getSecond() {
+        return second.get();
+    }
+
+    public ObjectProperty<Image> secondProperty() {
+        return second;
+    }
+
+    public void setSecond(Image second) {
+        this.second.set(second);
+    }
+
+    public Image getThird() {
+        return third.get();
+    }
+
+    public ObjectProperty<Image> thirdProperty() {
+        return third;
+    }
+
+    public void setThird(Image third) {
+        this.third.set(third);
+    }
+
+    public void smash(int lantern) {
+        //If image is not noSmash get a point and set image to noSmash
+
+
+    }
+
+    public void changeImage(int lantern, int imageId) {
+        switch (lantern) {
+            case 0 -> setFirst(getImage(imageId));
+            case 1 -> setSecond(getImage(imageId));
+            case 2 -> setThird(getImage(imageId));
+        }
+    }
+
+    private Image getImage(int imageId) {
+        return switch (imageId) {
+            case 0 -> smash1;
+            case 1 -> smash2;
+            case 2 -> smash3;
+            case 3 -> smash4;
+            default -> noSmash;
+        };
     }
 }
